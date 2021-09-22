f2bab0b06daf61f3d43ee9b421bc400771fed445
Louis Brent <louisbrent1992@gmail.com>
2021-09-21 22:28:10 -0700

    Recipe List with CSS done

---
+++ src/components/RecipeList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Recipe from "./Recipe";
+
+//Destructured 'recipes' from 'sampleRecipes' prop
+export default function RecipeList(props) {
+  const { recipes, handleRecipeAdd, handleRecipeDelete } = props
+  return (
+    <div className="recipe-list">
+      <div>
+        {recipes.map((recipe) => {
+          return <Recipe key={recipe.id} handleRecipeDelete={handleRecipeDelete} {...recipe} />;
+        })}
+      </div>
+      <div className="recipe-list__add-recipe-btn-container">
+        <button className="btn btn--primary" onClick={handleRecipeAdd}>
+          Add Recipe
+        </button>
+      </div>
+    </div>
+  );
+}

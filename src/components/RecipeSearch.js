a5dc70b08e9ba3a15e41845c87e39659a203ddbb
Louis Brent <louisbrent1992@gmail.com>
2022-01-11 14:16:51 -0800

    created recipe search component

---
+++ src/components/RecipeSearch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+// Filters recipes by ingredients
+
+function RecipeSearch() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RecipeSearch
